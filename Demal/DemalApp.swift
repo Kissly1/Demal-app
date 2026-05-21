ddb66320c7d14a34cfa721ee18bc0de2bbec7e99
Alexandr Kislitsin <a_kislitsin@kbtu.kz>
2026-05-21 15:35:12 +0500

    Initial Commit

---
+++ Demal/DemalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemalApp.swift
+//  Demal
+//
+//  Created by Alexandr Kisslitsyn on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DemalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
